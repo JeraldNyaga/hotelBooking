592627c2c4f2eaa0f3b7ee971bcce55d867c2aed
judgeOstrich <jeraldnyaga15@gmail.com>
2025-02-06 10:03:49 +0300

    Intial HTML structure

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+class Hotel {
+  constructor(name) {
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+}
+
+class HotelFeatures {
+    constructor(bedSize, bathType, concierge){
+        this.bedSize = bedSize;
+        this.bathType = bathType;
+        this.concierge = concierge;
+    }   
+}
+
+class Guest {}
+
+class GuestHotelBooking {}
+
+let novotelHotel = new Hotel("Novotel");

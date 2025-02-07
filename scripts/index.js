a253f20b2966663b43596abab752c898628dd89e
judgeOstrich <jeraldnyaga15@gmail.com>
2025-02-08 02:35:46 +0300

    Add dynamic hotel selection in booking

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,26 @@
-function displayHotel(){
-    
+function displayHotel() {
+  let mtKenya = document.getElementById("mtKenyaBut");
+  let mtElgon = document.getElementById("mtElgonBut");
+  let mtKasongo = document.getElementById("mtKasongoBut");
+
+  mtKenya.addEventListener("click", () => {
+    document.getElementById("mtElgonRes").classList.add("hidden");
+    document.getElementById("mtKasongoRes").classList.add("hidden");
+    document.getElementById("mtKenyaRes").classList.remove("hidden");
+  });
+  mtElgon.addEventListener("click", () => {
+    document.getElementById("mtKenyaRes").classList.add("hidden");
+    document.getElementById("mtKasongoRes").classList.add("hidden");
+    document.getElementById("mtElgonRes").classList.remove("hidden");
+  });
+  mtKasongo.addEventListener("click", () => {
+    document.getElementById("mtKenyaRes").classList.add("hidden");
+    document.getElementById("mtElgonRes").classList.add("hidden");
+    document.getElementById("mtKasongoRes").classList.remove("hidden");
+  });
 }
 
+displayHotel();
 class Hotel {
   constructor(name) {
     this.name = name;
@@ -13,11 +32,11 @@ class Hotel {
 }
 
 class HotelFeatures {
-    constructor(bedSize, bathType, concierge){
-        this.bedSize = bedSize;
-        this.bathType = bathType;
-        this.concierge = concierge;
-    }   
+  constructor(bedSize, bathType, concierge) {
+    this.bedSize = bedSize;
+    this.bathType = bathType;
+    this.concierge = concierge;
+  }
 }
 
 class Guest {}
